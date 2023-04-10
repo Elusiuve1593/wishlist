33e17e06c75e2dfeeb3527fd79fee041bb702156
Elusiuve1593 <elusiuve15.09.93@iclood.com>
2023-04-10 17:04:47 +0300

    Wish

---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,15 @@
+export const WishList = () => {
+
+    const onclickHandler = () => {
+
+    }
+    return (
+        <div>
+            <button className="text-3xl mt-8"
+                    onClick={onclickHandler}
+            >
+                Create an elemet
+            </button>
+        </div>
+    )
+}
--- src/Components/Wish.tsx
+++
@@ -1,7 +0,0 @@
-export const Wish = () => {
-    return (
-        <div>
-          <div className="text-3xl mt-8 underline text-red-800 justify-items-center">Welcome to Wish List!</div>
-        </div>
-    )
-}
